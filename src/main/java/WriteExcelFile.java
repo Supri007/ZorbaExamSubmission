b8fbb6d9899a7965cdd80e2d611e0a84effe947f
Supri <soup_rima07@hotmail.com>
2024-07-23 00:46:07 -0500

    Updated

--- src/main/java/WriteExcelFile.java
+++ src/main/java/WriteExcelFile.java
@@ -1,4 +1,3 @@
-import org.apache.poi.ss.formula.functions.Column;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
@@ -20,11 +19,17 @@ public class WriteExcelFile {
 
         System.out.println(xssfSheet.getLastRowNum() + 1);
         int newRowCount = xssfSheet.getLastRowNum() + 1;
-        Row row = xssfSheet.getRow(1);
-        int cellCount = row.getLastCellNum();
+        Row row1 = xssfSheet.getRow(1);
+        Row row2 = xssfSheet.getRow(2);
+        Row row3 = xssfSheet.getRow(3);
+        Row row4 = xssfSheet.getRow(4);
+        Row row5 = xssfSheet.getRow(5);
+        Row row6 = xssfSheet.getRow(6);
+        Row row7 = xssfSheet.getRow(7);
+        int cellCount = row1.getLastCellNum();
+        System.out.println("Cell count: " + cellCount);
         int totalCellCount = cellCount + 3;
-        //Create new row
-        Row newRow = xssfSheet.createRow(cellCount);
+
         Cell cell = null;
 
         for (int i = cellCount; i < totalCellCount; i++)
@@ -32,54 +37,54 @@ public class WriteExcelFile {
             String colName = xssfSheet.getRow(0).getCell(i).getStringCellValue();
             System.out.println(colName);
             System.out.println(i);
-            for (int j = i; j < 3; j++){
+            for (int j = i; j < i + 3; j++){
                 switch(j){
-                    case 6:
-                        cell = newRow.createCell(j, CellType.STRING);
+                    case 5:
+                        cell = row1.createCell(j, CellType.STRING);
                         cell.setCellValue("Null");
-                        cell = newRow.createCell(j, CellType.STRING);
+                        cell = row2.createCell(j, CellType.NUMERIC);
                         cell.setCellValue(1001);
-                        cell = newRow.createCell(j, CellType.STRING);
+                        cell = row3.createCell(j, CellType.NUMERIC);
                         cell.setCellValue(1004);
-                        cell = newRow.createCell(j, CellType.STRING);
+                        cell = row4.createCell(j, CellType.NUMERIC);
                         cell.setCellValue(1004);
-                        cell = newRow.createCell(j, CellType.STRING);
+                        cell = row5.createCell(j, CellType.NUMERIC);
                         cell.setCellValue(1001);
-                        cell = newRow.createCell(j, CellType.STRING);
+                        cell = row6.createCell(j, CellType.NUMERIC);
                         cell.setCellValue(1005);
-                        cell = newRow.createCell(j, CellType.STRING);
+                        cell = row7.createCell(j, CellType.NUMERIC);
                         cell.setCellValue(1001);
+                        break;
+                    case 6:
+                        cell = row1.createCell(j, CellType.STRING);
+                        cell.setCellValue("Finance");
+                        cell = row2.createCell(j, CellType.STRING);
+                        cell.setCellValue("Finance");
+                        cell = row3.createCell(j, CellType.STRING);
+                        cell.setCellValue("R&D");
+                        cell = row4.createCell(j, CellType.STRING);
+                        cell.setCellValue("R&D");
+                        cell = row5.createCell(j, CellType.STRING);
+                        cell.setCellValue("Finance");
+                        cell = row6.createCell(j, CellType.STRING);
+                        cell.setCellValue("Finance");
+                        cell = row7.createCell(j, CellType.STRING);
+                        cell.setCellValue("Finance");
                         break;
                     case 7:
-                        cell = newRow.createCell(j, CellType.STRING);
-                        cell.setCellValue("Finance");
-                        cell = newRow.createCell(j, CellType.STRING);
-                        cell.setCellValue("Finance");
-                        cell = newRow.createCell(j, CellType.STRING);
-                        cell.setCellValue("R&D");
-                        cell = newRow.createCell(j, CellType.STRING);
-                        cell.setCellValue("R&D");
-                        cell = newRow.createCell(j, CellType.STRING);
-                        cell.setCellValue("Finance");
-                        cell = newRow.createCell(j, CellType.STRING);
-                        cell.setCellValue("Finance");
-                        cell = newRow.createCell(j, CellType.STRING);
-                        cell.setCellValue("Finance");
-                        break;
-                    case 8:
-                        cell = newRow.createCell(j, CellType.NUMERIC);
+                        cell = row1.createCell(j, CellType.NUMERIC);
                         cell.setCellValue(60);
-                        cell = newRow.createCell(j, CellType.NUMERIC);
+                        cell = row2.createCell(j, CellType.NUMERIC);
                         cell.setCellValue(20);
-                        cell = newRow.createCell(j, CellType.NUMERIC);
+                        cell = row3.createCell(j, CellType.NUMERIC);
                         cell.setCellValue(30);
-                        cell = newRow.createCell(j, CellType.NUMERIC);
+                        cell = row4.createCell(j, CellType.NUMERIC);
                         cell.setCellValue(40);
-                        cell = newRow.createCell(j, CellType.NUMERIC);
+                        cell = row5.createCell(j, CellType.NUMERIC);
                         cell.setCellValue(20);
-                        cell = newRow.createCell(j, CellType.NUMERIC);
+                        cell = row6.createCell(j, CellType.NUMERIC);
                         cell.setCellValue(15);
-                        cell = newRow.createCell(j, CellType.NUMERIC);
+                        cell = row7.createCell(j, CellType.NUMERIC);
                         cell.setCellValue(25);
                         break;
                 }
